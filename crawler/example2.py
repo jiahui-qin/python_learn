a5d2729b273a3a09305d5d143908caebd3079ba0
saber <feverqjh@outlook.com>
2017-10-10 00:31:11 +0800

    爬糗事百科前十页的内容

--- crawler/example2.py
+++ crawler/example2.py
@@ -1,34 +1,22 @@
 #模拟浏览器发送get请求，需要使用request对象
 #往request对象添加http头，就可以把请求伪装成浏览器
 ##查看报头(header)？
+##使用更加先进的 beautifulsoup果然十分强大，完美避免正则表达式的困扰
+##http://beautifulsoup.readthedocs.io/zh_CN/latest/#find-all
+##http://cuiqingcai.com/1319.html
 
-# from urllib import request
-
-# req = request.Request('http://www.163.com/')
-# req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
-# with request.urlopen(req) as f:
-#     data=f.read() ## .read()方法可以读取
-#     print('Status:',f.status, f.reason) ## .status\.reason 是读取之后返回的状态信息
-#     for k,v in f.getheaders():
-#         print('%s: %s' % (k, v))
-
-#     print('data: ',data.decode('utf-8'))
-
-
+##爬糗事百科前十页的内容
 from urllib import request
-import re
-
-url=request.Request("http://www.qiushibaike.com/hot/page/1/")
-url.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
-with request.urlopen(url) as f:
-    #req = request.Request(url)
-    #data=request.urlopen(url)
-    data=f.read().decode('utf-8')
-    print(data)
-    pattern = re.compile(r'class="text"([\s\S]*)sapn?',re.S)
-    items = re.findall(pattern,data)
-    for item in items:
-        print(item)
-
-
-    #<a href="/article/119603372" class="text" onclick="_hmt.push(['_trackEvent', 'list-qiushi-first', 'chick']);">
+from bs4 import BeautifulSoup
+for i in range(1,10):
+    url=request.Request("http://www.qiushibaike.com/hot/page/"+str(i)+"/")
+    url.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.')
+    with request.urlopen(url) as f:
+        data=f.read().decode('utf-8')    
+        #pattern = re.compile(r'class="text"([\s\S]*)sapn?',re.S)
+        #items = re.findall(pattern,data) #失败的正则表达式尝试
+        soup = BeautifulSoup(data, 'html.parser')
+        #print(soup.prettify())
+        for i in soup.find_all("a",class_="contentHerf"):
+            print(i.get_text(strip=True)) #用get_text方法可以提取当前tag包括子孙tag中的text内容
+            print()
