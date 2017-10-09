b0f6f7639c221c94e7033c68c0db84f49d770b23
saber <feverqjh@outlook.com>
2017-10-09 10:58:50 +0800

    request.urlopen的使用

--- crawler/example1.py
+++ crawler/example1.py
@@ -2,41 +2,16 @@
 #对于一些需要登录的网站，如果不是从浏览器发出的请求，则得不到响应。所以，我们需要将爬虫程序发出的请求伪装成浏览器正规军。
 #具体实现：自定义网页请求报头。
 ##如何查看请求和响应报头？
-import urllib.request,socket,re,sys,os  
-  
-#定义文件保存路径  
-targetPath = "E:\\Spider"  
-  
-def saveFile(path):  
-    #检测当前路径的有效性  
-    if not os.path.isdir(targetPath):  
-        os.mkdir(targetPath)  
-  
-    #设置每个图片的路径  
-    pos = path.rindex('/')  
-    t = os.path.join(targetPath,path[pos+1:])  
-    return t  
-  
-#用if __name__ == '__main__'来判断是否是在直接运行该.py文件  
-  
-  
-# 网址  
-url = "https://avmo.club/cn"  
-headers = {  
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '                            'Chrome/51.0.2704.63 Safari/537.36'  
-           }  
-  
-req = urllib.request.Request(url=url, headers=headers)  
-  
-res = urllib.request.urlopen(req)  
-  
-data = res.read()  
 
-print(data)
-for link,t in set(re.findall(r'(https:[^s]*?(jpg|png|gif))', str(data))):  
-  
-    print(link)  
-    try:  
-        urllib.request.urlretrieve(link,saveFile(link))  
-    except:  
-        print('失败')  
+#爬虫的实现方式先学习urllib和正则表达式
+#熟练之后再学习requests和beautifulSoup
+from urllib import request 
+
+with request.urlopen('https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001432688314740a0aed473a39f47b09c8c7274c9ab6aee000/') as f:
+    ##用request.urlopen('')来读取网页中的数据
+    data=f.read() ## .read()方法可以读取
+    print('Status:',f.status, f.reason) ## .status\.reason 是读取之后返回的状态信息
+    for k,v in f.getheaders():
+        print('%s: %s' % (k, v))
+
+    print('data: ',data.decode('utf-8'))
