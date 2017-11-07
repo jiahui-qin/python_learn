70f0486253e2a612af238adeac612d60f64a435f
saber <feverqjh@outlook.com>
2017-11-07 17:18:17 +0800

    avmmm

--- crawler/avmoo.py
+++ crawler/avmoo.py
@@ -1,6 +1,49 @@
 from urllib import request
 from bs4 import BeautifulSoup
-data = request.Request("https://avmo.club/cn")
+from time import sleep
+import re
+
+def movieinfo(url):
+    data = request.Request(url)
+    data.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36')
+    data.add_header('Upgrade-Insecure-Requests',1)
+    with request.urlopen(data) as f:
+        data=f.read().decode('utf-8')
+        soup = BeautifulSoup(data, 'html.parser')
+        actors = soup.find_all('a', class_ = 'avatar-box')
+        print('actors:', end=' ')
+        for actor in actors:
+            print(actor.get_text(strip=True), end=' ')
+        print('\n',end='')
+        tags = soup.find_all('span', class_ = 'genre')
+        print('tags:', end=' ')
+        for tag in tags:
+            print(tag.get_text(strip = True),end=' ')
+        print('\n',end='')
+        factory = soup.find('a', href=re.compile("studio"))
+        print("producer:" + factory.get_text(strip=True))
+        Idcode = soup.find('span', style="color:#CC0000;")
+        moviedownload(Idcode.get_text())
+
+def moviedownload(idcode):
+    Dowpage = request.Request('https://btso.pw/search/' + idcode)
+    Dowpage.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36')
+    Dowpage.add_header('Refer', 'https://btso.pw/search/' + idcode)
+    Dowpage.add_header('Accept-Encoding', 'gzip, deflate, br')
+    Dowpage.add_header('Upgrade-Insecure-Requests', '1')
+    Dowpage.add_header('Accept-Language', 'zh-CN,zh;q=0.8')
+
+    with request.urlopen(Dowpage) as dpage:
+        dpage = dpage.read().decode('utf-8')
+        dsoup = BeautifulSoup(dpage, 'html.parser')
+        flag = dsoup.find('div', text = "Torrent Description")
+        if flag:
+            print("Dowland page: "+'https://btso.pw/search/' + idcode)
+        else:
+            print("btso暂无" + idcode + '下载地址')
+
+        
+data = request.Request("https://avmo.club/cn/search/VENUS/page/3")
 data.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36')
 data.add_header('Referer','https://avmo.club/cn')
 data.add_header('Upgrade-Insecure-Requests',1)
@@ -11,5 +54,7 @@ with request.urlopen(data) as f:
         for text in movie.stripped_strings:
             if text != '/':
                 print(text)
-        print(movie.parent.parent.a['href'])
+        #print(movie.parent.parent.a['href'])
+        movieinfo(movie.parent.parent.a['href'])
         print()
+
--- crawler/avmoo1.py
+++ crawler/avmoo1.py
@@ -1,14 +1,23 @@
 from urllib import request
 from bs4 import BeautifulSoup
-data = request.urlopen("file:///C:/Users/saber/Desktop/avmp1.html")
-data=data.read().decode('utf-8')
-soup = BeautifulSoup(data, 'html.parser')
-tags = soup.find('div', class_ = "col-md-3 info")
-print(soup.find('div',id="avatar-waterfall").get_text(strip=True))
-for tag in tags.stripped_strings:
-    ##这里可以通过匹配':'来识别属性
-    if list(tag).pop()==':':
-        print(tag,end=' ')
-    else:
-        print(tag)
-        print(tag)
+import re
+data = request.Request("https://avmo.club/cn/movie/6b0g")
+data.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36')
+data.add_header('Upgrade-Insecure-Requests',1)
+with request.urlopen(data) as f:
+    data=f.read().decode('utf-8')
+    soup = BeautifulSoup(data, 'html.parser')
+    actors = soup.find_all('a', class_ = 'avatar-box')
+    print('actors:', end=' ')
+    for actor in actors:
+        print(actor.get_text(strip=True), end=' ')
+    print('\n',end='')
+    tags = soup.find_all('span', class_ = 'genre')
+    print('tags:', end=' ')
+    for tag in tags:
+        print(tag.get_text(strip = True),end=' ')
+    print('\n',end='')
+    factory = soup.find('a', href=re.compile("studio"))
+    print("producer:" + factory.get_text(strip=True))
+    Idcode = soup.find('span', style="color:#CC0000;")
+    print(Idcode.get_text())
