3dfb02729e430672ba734225ccb70597cdb81487
saber <feverqjh@outlook.com>
2017-12-28 19:58:18 +0800

    !

--- crawler/cosx_archives.py
+++ crawler/cosx_archives.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from urllib import request
-import time
+from time import sleep
 from bs4 import BeautifulSoup
-import re
 import pymysql
+import io
+import sys
+sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 
 def create_table():
     #创建了一个table用来储存信息
@@ -11,11 +13,11 @@ def create_table():
     cursor = db.cursor()
     cursor.execute("create table article(\
         id int unsigned not null auto_increment primary key,\
-        time char(8) not null, \
-        title char(50) not null, \
-        keyword char(100) not null, \
-        author char(30) not null, \
-        address char(70) not null);")  
+        time char(20) not null, \
+        title char(120) not null, \
+        keyword char(120) , \
+        author char(120) not null, \
+        address char(120) not null);")  
     db.commit()
     db.close()
     print('数据库创建成功！')
@@ -31,7 +33,10 @@ def get_article(url):
         target = article_soup.find('div', class_ = 'article-meta')
         title = article_soup.find('span', class_ = 'title').get_text()
         author = article_soup.find('h3', class_ = 'author').get_text()
-        keyword = target.p.get_text()
+        if not target.p:
+            keyword = 'None_keyword'
+        else:
+            keyword = target.p.get_text()
         return [title,author, keyword]
 
 def main():
@@ -39,7 +44,7 @@ def main():
     cosx_url = request.Request('https://cosx.org/archives/')
     cosx_url.add_header('Refer','https://coxs.org/')
     cosx_url.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36')
-    db= pymysql.connect(host="localhost",user="root", password="1234", db = 'cosx')
+    db= pymysql.connect(host="localhost",user="root", password="1234", db = 'cosx', charset="utf8")
     cursor = db.cursor()
     with request.urlopen(cosx_url) as f:
         f = f.read().decode('UTF-8')
@@ -47,17 +52,21 @@ def main():
         cosx = fsoup.find('main')
         articles = cosx.find_all('li')
         for article in articles:
-            print(article)
             url = article.a['href']
             get_a = get_article(url)
             title = get_a[0]
+            print('now loading: ' + title)
             author = get_a[1][0:-1]
-            keyword = get_a[2][4,-1]
+            if get_a[2] != 'None_keyword':
+                keyword = get_a[2][4:-1]
+            else:
+                keyword = get_a[2] 
             time = article.span.get_text()
+            print(time)
             address = article.a['href']
-            cursor.execute("insert into article values[NULL, time, title, keyword, author, address];")
+            cursor.execute("insert into article values(NULL, '%s', '%s', '%s', '%s', '%s')" % (time, title, keyword, author, address))
             db.commit()
-            time.sleep(1)
+            sleep(1)
     db.close()
 
 
