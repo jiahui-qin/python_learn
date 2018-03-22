1ba84d78bde9e13174c4b28a855de6ebe7971f9d
saber <feverqjh@outlook.com>
2018-03-22 23:56:11 +0800

    明天整理一下美团的笔试

--- crawler/test.py
+++ crawler/test.py
@@ -1,3 +1,15 @@
-a = [3,2,1]
-a.pop(1)
-print(a)
+if '0' not in test:
+    print(0)
+
+countt = []
+for i in range(10):
+    countt.append(test.count(str(i)))
+
+j = 1
+while 1:
+    jj = str(j)
+    for jjj in jj:
+        p = jj.count(jjj)
+        if countt[num(jjj)] < p:
+            print(j)
+            break
